5c15c527351f43c31c3060db1fb053e8bbcfd216
rhettman360 <rhettman360@gmail.com>
2017-10-09 16:55:06 -0700

    finished mongoose dashboard

--- MONGODB/basic_mongoose_app/server.js
+++ MONGODB/basic_mongoose_app/server.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/basic_mongoose');
 var UserSchema = new mongoose.Schema({
-  name: String,
-  age: Number
-})
+  name: { type:String, required: true, minlength:3},
+  age: { type:Number, min: 1, max: 150}
+}, {timestamps: true })
 mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
 var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
 mongoose.Promise = global.Promise;
@@ -32,20 +32,32 @@ app.get('/', function(req, res) {
     })
 })
 // Add User Request
-app.post('/users', function(req, res) {
-    console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    var user = new User({name: req.body.name, age: req.body.age});
-    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-    user.save(function(err) {
-    // if there is an error console.log that something went wrong!
-      if(err) {
-        console.log('something went wrong');
-      } else { // else console.log that we did well and then redirect to the root route
-        console.log('successfully added a user!');
-        res.redirect('/');
-      }
-   })
+// app.post('/users', function(req, res) {
+//     console.log("POST DATA", req.body);
+//     // This is where we would add the user from req.body to the database.
+//     var user = new User({name: req.body.name, age: req.body.age});
+//     // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+//     user.save(function(err) {
+//     // if there is an error console.log that something went wrong!
+//       if(err) {
+//         console.log('something went wrong', user.errorsd);
+//         res.render('index', {errors: user.errors});
+//       } else { // else console.log that we did well and then redirect to the root route
+//         console.log('successfully added a user!');
+//         res.redirect('/');
+//       }
+//    })
+// })
+app.post('/users', function (req, res){
+    var user = new User(req.body);
+    user.save(function(err){
+        if(err){
+            res.render('index', {errors: user.errors})
+        }
+        else {
+            res.redirect('/users');
+        }
+    });
 })
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
